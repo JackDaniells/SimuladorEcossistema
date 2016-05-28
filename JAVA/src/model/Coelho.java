d729edb6f9e970329cbba67c4815000bb9ea482a
jackdaniells <danielk.kock@live.com>
2016-05-28 01:50:23 -0300

    Levantamento de requisitos e criação das classes Lobo e Coelho

---
+++ JAVA/src/model/Coelho.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Coelho{
+	
+	private int idadeFertil = 5;
+	private int idadeMax = 15;
+	private int fome = 0;
+	private String sexo;
+	private String nome;
+	
+	
+	public Coelho( String nome, String sexo) {
+		this.nome = nome;
+		this.sexo = sexo;
+		
+	}
+
+	
+	
+
+}
---
+++ JAVA/src/model/Lobo.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Lobo {
+	
+	private int idadeFertil = 10;
+	private int idadeMax = 20;
+	private int fome = 0;
+	private String sexo;
+	private String nome;
+	
+	
+	public Lobo(String nome, String sexo) {
+		this.nome = nome;
+		this.sexo = sexo;
+		
+	}
+
+	
+
+}
